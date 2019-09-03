a2d64fc9f7d0832fdb1e56f696a971b476de99c2
uia55223 <uia55223@TMDG849W.cw01.contiwan.com>
2019-09-03 11:10:58 +0300

    Injecting literals

--- spring-demo-one/src/springdemo/CricketCoach.java
+++ spring-demo-one/src/springdemo/CricketCoach.java
@@ -3,6 +3,8 @@ package springdemo;
 public class CricketCoach implements Coach {
 	
 	private FortuneService fortuneService;
+	private String emailAddress;
+	private String team;
 
 	// create a no-arg constructor
 	public CricketCoach() {
@@ -24,5 +26,24 @@ public class CricketCoach implements Coach {
 		System.out.println("Inside setter method");
 		this.fortuneService = fortuneService;
 	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("Inside email setter method");
+		this.emailAddress = emailAddress;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("Inside team setter method");
+		this.team = team;
+	}
+	
 
 }
